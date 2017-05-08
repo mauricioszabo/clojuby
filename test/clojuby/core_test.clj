d28bb4b391e602ac26cd1039a97a50070f8ab3b1
Maurício Szabo <mauricio.szabo@gmail.com>
2017-05-07 23:47:12 -0300

    Super method test

--- test/clojuby/core_test.clj
+++ test/clojuby/core_test.clj
@@ -38,11 +38,21 @@
     (rb/public-send "to_s" 10) => "10"
     (rb/public-send "to_s" 10 16) => "a")
 
-  (fact "creates classes"
-    (let [class (rb/new-class* {"sum_two" (fn [_ a b] (+ a b))})
-          instance (rb/new class)]
-      (rb/public-send "sum_two" instance 10 20) => 30)
+  (facts "about class creation"
+    (fact "creates simple class"
+      (let [class (rb/new-class* {"sum_two" (fn [_ a b] (+ a b))})
+            instance (rb/new class)]
+        (rb/public-send "sum_two" instance 10 20) => 30))
 
-    (let [class (rb/new-class* (rb/eval "String") {"append" (fn [self a] (str self "-" a))})
-          instance (rb/new class "some-str")]
-      (rb/public-send "append" instance "foo") => "some-str-foo")))
+    (fact "calls methods refering to self"
+      (let [class (rb/new-class* (rb/eval "String") {"append"
+                                                     (fn [self a] (str self "-" a))})
+            instance (rb/new class "some-str")]
+        (rb/public-send "append" instance "foo") => "some-str-foo"))
+
+    (fact "refers to 'super'"
+      (let [class (rb/new-class* (rb/eval "String")
+                                 {"upcase" (rb/defn []
+                                             (str "-" (super) "-" self))})
+            instance (rb/new class "str")]
+        (rb/public-send "upcase" instance) => "-STR-str"))))
